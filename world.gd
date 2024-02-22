9db61dad8866765bf7c8dcddd62c5bb08ff088f6
tobynk <122053994+tobynk@users.noreply.github.com>
2024-02-22 15:47:45 -0800

    dojne

--- world.gd
+++ world.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 @export var next_level: PackedScene
 
+var level_time = 0.0
+var start_level_msec = 0.0
+
 @onready var level_completed = $CanvasLayer/LevelCompleted
 @onready var start_in_label = %StartInLabel
 @onready var start_in = %StartIn
 @onready var animation_player = $AnimationPlayer
+@onready var level_time_label = %LevelTimeLabel
 
 func _ready():
 	Events.level_completed.connect(show_level_completed)
@@ -16,6 +20,11 @@ func _ready():
 	await animation_player.animation_finished
 	get_tree().paused = false
 	start_in.visible = false
+	start_level_msec = Time.get_ticks_msec()
+
+func _process(delta):
+	level_time = Time.get_ticks_msec() - start_level_msec
+	level_time_label.text = str(level_time/1000.0)
 	
 func show_level_completed():
 	level_completed.show()
